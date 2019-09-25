f22cdfddd009ab8122bddb6886be058d30e17927
Charles Chong <charleschong@Charless-MacBook-Pro.local>
2019-09-24 20:16:36 -0700

    refactor(adjusting api to receive challenge): adjusted api to receive challenge

--- app_routes/academy.py
+++ app_routes/academy.py
@@ -9,6 +9,7 @@ from initialize.academy import main
 academy_parser = reqparse.RequestParser()
 academy_parser.add_argument('courseId', help='{error_msg}')
 academy_parser.add_argument('courseHandle', help='{error_msg}')
+academy_parser.add_argument('data', help='{error_msg}')
 
 categories, courses = main()
 print(courses,categories)
@@ -22,7 +23,6 @@ class apiAcademyCourses(Resource):
             return 404
 
 class apiAcademyCourse(Resource):
-    
     def get(self):
         args = academy_parser.parse_args()
         try:
@@ -30,6 +30,37 @@ class apiAcademyCourse(Resource):
                 print(i["id"],args['courseId'])
                 if i["id"] == args['courseId']:
                     return i, 201 ,  {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Methods": "GET"} 
+        except:
+            return 404
+
+class apiAcademyCourseSave(Resource):
+    def post(self):
+        args = academy_parser.parse_args()
+        data = args['data']
+        course = None
+        try:
+            for i in courses:
+                if i["id"] == data["id"]:
+                    course = data
+                    return course, 201 ,  {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Methods": "POST"} 
+                return course, 201 ,  {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Methods": "POST"} 
+            if course is not None:
+                course = data
+                courses.append(course)
+            return course, 201 ,  {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Methods": "POST"} 
+        except:
+            return 404
+
+class apiAcademyCourseUpdate(Resource):
+    def post(self):
+        args = academy_parser.parse_args()
+        data = args["data"]
+        try:
+            for i in courses:
+                if i["id"] == data["id"]:
+                    return courses.update(data), 201 ,  {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Methods": "POST"} 
+                else:
+                    return i
         except:
             return 404
 
--- initialize/academy.py
+++ initialize/academy.py
@@ -4,18 +4,31 @@ from app_controllers.academy.courses import Courses
 from app_controllers.academy.course import Course
 from app_controllers.academy.steps import Steps
 from app_controllers.academy.step import Step
-import json
+
 def main():
-    category = Category()
-    category.setCategory(0,'web','Web','#3f51b5')
+    category1 = Category()
+    category2 = Category()
+    category3 = Category()
+    category4 = Category()
+    category1.setCategory(0,'web','Web','#3f51b5')
+    category2.setCategory(1,'aws','AWS','#3f51b5')
+    category3.setCategory(2,'gcp','GCP','#3f51b5')
+    category4.setCategory(3,'cicd','CI-CD','#3f51b5')
     categories = Categories()
-    categories.addCategory(category.to_dict()) 
-    course = Course()
+    categories.addCategory(category1.to_dict()) 
+    categories.addCategory(category2.to_dict()) 
+    categories.addCategory(category3.to_dict()) 
+    categories.addCategory(category4.to_dict()) 
+    
+    course1 = Course()
     steps = Steps()
-    step = Step()
-    step.setStep('0','Overview','<h1>Overview</h1>')
-    steps.addStep(step.to_dict())
-    course.setCourse(
+    step1 = Step()
+    step1.setStep('0','Overview','<h1>Overview</h1>')
+    step2 = Step()
+    step2.setStep('1','Overview','<h1>2</h1>')
+    steps.addStep(step1.to_dict())
+    steps.addStep(step2.to_dict())
+    course1.setCourse(
         '15459251a6d6b397565',
         'Challenge 1',
         'challenge-1',
@@ -26,7 +39,7 @@ def main():
         0,
         steps.getSteps())
     courses = Courses()
-    courses.addCourse(course.to_dict())
+    courses.addCourse(course1.to_dict())
     return categories.getCategories(),courses.getCourses()
 
 
